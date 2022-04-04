0a54780e83f445629603e4cb172e667a093448e3
Juan Carlos Suarez <juancasy@gmail.com>
2022-04-05 01:56:37 +0200

    Include references to n_minmax and n_maxmin in both the description and the @seealso section of n_range

--- R/n_range.R
+++ R/n_range.R
@@ -1,7 +1,8 @@
 #' n_range
 #'
 #' \code{n_range} determines the common n range for all the L in the
-#'     oscillation spectrum as n = [n_maxmin, n_minmax]
+#'     oscillation spectrum as
+#'     n = (\code{\link{n_maxmin}}, \code{\link{n_minmax}})
 #'
 #'
 #' This function takes one argument, a dataframe with the synthetic oscillation
@@ -19,6 +20,7 @@
 #' \dontrun{
 #'   n_range(df_olscillations)
 #' }
+#' @seealso \code{\link{n_maxmin}}, \code{\link{n_minmax}}
 
 n_range <- function(model.df) {
   n_high <- n_minmax(model.df)
