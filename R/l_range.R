0d10b13963a49ab5fa0afa1e902ee31781856365
Juan Carlos Suárez Yanes <jcsuarez@festelar8.ugr.es>
2022-04-05 20:16:39 +0200

    Include new function: l_range Include doc for l_range

---
+++ R/l_range.R
@@ -0,0 +1,29 @@
+#' l_range
+#'
+#' \code{l_range} determines the common L range for all the
+#'     oscillation spectrum as
+#'     n = (L_min, L_max)
+#'
+#'
+#' This function takes one argument, a dataframe with the synthetic oscillation
+#' spectra read from read_graco, read_adipls, read_gyre, or read_losc.
+#'
+#'
+#' @param model.df DataFrame.
+#' @return It returns a vector with c(l_min, l_max)
+#'
+#'
+#'
+#' @examples
+#' range <- l_range(df_oscillations)
+#'
+#' \dontrun{
+#'   l_range(df_oscillations)
+#' }
+#' @seealso \code{\link{n_range}}
+
+l_range <- function(model.df) {
+  l_high <- max(model.df$l)
+  l_low <- min(model.df$l)
+  lrange <- c(l_low, l_high)
+}
