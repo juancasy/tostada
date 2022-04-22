d7ae06b4301135f79d2f78f95362ba7887bde6f7
Juan Carlos Suárez Yanes <jcsuarez@Phobos.local>
2022-04-22 13:11:56 +0200

    Uptdate package from 0.1.0.9001  to 0.1.0.9002 Update read_cestam.R (status: running without errors)- require full verification Update doc for read_cestam.R

--- R/read_cestam.R
+++ R/read_cestam.R
@@ -9,10 +9,56 @@
 #' of CESAM2K models as well as MESA (OSC-type) models
 #'
 #'
-#' @param file ASCII file (.osc or .OSC)
+#'@section Metadata:
+#'
+#' \tabular{ll}{
+#'    \code{name} \tab  name of the model file \cr
+#'    \code{code} \tab  generator code name \cr
+#'    \code{version} \tab  version of the code \cr
+#'    \code{date} \tab date of the computation \cr
+#'    \code{time} \tab time of the computation  \cr
+#'  }
+#'
+#'
+#'@section Global variables:
+#'
+#' \tabular{ll}{
+#'    \code{rmass} \tab  mass, cgs \cr
+#'    \code{R} \tab  radius, cgs \cr
+#'    \code{rphot} \tab  photometric radius, cgs \cr
+#'    \code{rlum} \tab luminosity, cgs  \cr
+#'    \code{al} \tab convective alpha (dimensionless) \cr
+#'    \code{age} \tab age, Myr \cr
+#'    \code{rotini} \tab initial angular rotation (in rad/s) \cr
+#'    \code{rlogg} \tab surface gravity, cgs, log \cr
+#'    \code{teff} \tab effective temperature (temp(r=rphot)), K \cr
+#'    \code{nshell} \tab number of shells of the model (radial)
+#'  }
+#'
+#' @section Structure variables:
+#'
+#' \tabular{ll}{
+#'    \code{r} \tab  radius profile, cgs, log \cr
+#'    \code{q} \tab  mass profile, cgs, log \cr
+#'    \code{temp} \tab temperature profile, K  \cr
+#'    \code{p} \tab pressure profile, cgs \cr
+#'    \code{rho} \tab density profile rho, cgs \cr
+#'    \code{gamma} \tab gamma1 parameter, cgs \cr
+#'    \code{aast} \tab A constant (BV) \cr
+#'    \code{rr} \tab radius normalized to total radius \cr
+#'    \code{c1} \tab solar dynamical time \cr
+#'    \code{cs2} \tab sound speed squared, cgs \cr
+#'    \code{vg} \tab solar dynamical time \cr
+#'    \code{rot} \tab rotation profile, rad/s \cr
+#'
+#'  }
+#'
+#' @param file ASCII file with a stellar structure compliant with stellar
+#' oscillations computation with extension .osc or .OSC.
 #' @return A dataframe with the stellar structure, in which global parameters
-#' - common values for the star - are saved as attributes, and variables -
+#' -common values for the star - are saved as attributes, and variables -
 #' physical quantities that vary with the stellar depth- are saved as columns.
+#'
 #'
 #'
 #'
@@ -38,13 +84,12 @@ read_cestam <- function(strFile){
       # remove first blank space in line
       s <- sub("^\\s+", "", i)
       # separate the  - that sometimes join different numbers
-      v <-
-        as.numeric(unlist(strsplit(
+      v <- as.numeric(unlist(strsplit(
           gsub("([0-9])-([0-9])", "\\1 -\\2", s), " "
         )))
       dat <- append(dat, v)
     }
-    return(datos)
+    return(dat)
   }
 
   # Open file for sequential read
@@ -54,7 +99,7 @@ read_cestam <- function(strFile){
   headerlines <- readLines(con = cfile, n = 6)
   # Establish whether adiabatic or non-adiabatic
   noAdiabaticas <- grepl("non adiabatiques", headerlines[1])
-  linea2 <- strsplit(linea[2]," ")
+  linea2 <- strsplit(headerlines[2]," ")
   driver <- linea2[[1]][1]
   version <- linea2[[1]][3]
   contenido <- NA
@@ -64,19 +109,21 @@ read_cestam <- function(strFile){
   if(noAdiabaticas){
     contenido <- "Non Adiabatic"
     # define indexes of variable for the non-adiabatic case
-    indices <- c(rmasa = 1, rfotom = 2, rlumi = 3, al = 6, edad = 11, rot = 12,
-                 r = 1, q = 2, t = 3, p = 4, rho = 5, gamma = 10, aast = 15)
+    indices <- c(rmass = 1, rfotom = 2, rlum = 3, al = 6, age = 11, rotini = 12,
+                 r = 1, q = 2, t = 3, p = 4, rho = 5, gamma = 10, aast = 15,
+                 rot = 16)
   }else{
     contenido <- "Adiabatic"
     # define indexes of variable for the adiabatic case
-    indices <- c(rmasa = 1, rfotom = 2, rlumi = 3, al = 6, edad = 11, rot = 13,
-                 r = 1, q = 2, t = 3, p = 4, rho = 5, gamma = 10, aast = 15)
+    indices <- c(rmass = 1, rfotom = 2, rlum = 3, al = 6, age = 11, rotini = 13,
+                 r = 1, q = 2, t = 3, p = 4, rho = 5, gamma = 10, aast = 15,
+                 rot = 16)
   }
 
 
 
   # Read array of relevant numbers in the model (nshell, nchim, ncz, et.)
-  numeros <- strsplit(linea[6]," {1,20}")[[1]]
+  numeros <- strsplit(headerlines[6]," {1,20}")[[1]]
   # Remove the last shell (center of star) to avoid numerical problems.
   m <- as.integer(numeros[2])-1
   # No. of data included in global data
@@ -87,34 +134,84 @@ read_cestam <- function(strFile){
   numDatosCapa <- as.integer(numeros[4]) + numElemQuim
 
   # Create a structure for the model
-  e <- data.frame(r = double(m),
-                  q = double(m),
-                  temp = double(m),
-                  p = double(m),
-                  rho = double(m),
-                  gamma = double(m),
-                  aast = double(m),
-                  teff = double(m),
-                  rr = double(m),
-                  c1 = double(m),
-                  cs2 = double(m),
-                  vg = double(m),
-                  u = double(m)
+  e <- data.frame(r = double(m),     # r*rsol
+                  q = double(m),     # log(m/mstar) -1.d38 au centre
+                  temp = double(m),     # temparature profile
+                  p = double(m),     # pressure profile
+                  rho = double(m),     # density profile
+                  gamma = double(m),     #
+                  aast = double(m),     #
+                  rr = double(m),     #
+                  c1 = double(m),     #
+                  cs2 = double(m),     #
+                  vg = double(m),     #
+                  u = double(m),     #
+                  rot = double(m)     #
   )
 
+  #temp <- double(m)
   # Load global data
   # block has lines of 5 data each with a total of numDatosGlobales
   datos <- readBlock(cfile, ceiling(numDatosGlobales/5))
   # The no. of data included change with Cesam version. Frozen from CESAM2k
 
   # Store global data as attributes
-  attr(e,"rmasa") <- datos[indices["rmasa"]]
+  attr(e,"rmass") <- datos[indices["rmass"]]
   attr(e,"rfotom") <- datos[indices["rfotom"]]
-  attr(e,"rlumi") <- log(datos[indices["rlumi"]]/Constants["slumi"])
-  attr(e,"al") <- datos[indices["al"]]
-  attr(e,"edad") <- datos[indices["edad"]]
-  attr(e,"rot") <- datos[indices["rot"]]
-  attr(e,"rlogg") <- log((Constants["gk"]*e$rmasa)/(e$rfotom^2))
+  attr(e,"rlum") <- log(datos[indices["rlum"]]/constants$slum)
+  attr(e,"alpha") <- datos[indices["al"]]
+  attr(e,"age") <- datos[indices["age"]]
+  attr(e,"rotini") <- datos[indices["rotini"]]
+  attr(e,"rlogg") <- log((constants$gk*attributes(e)$rmass)/(attributes(e)$rfotom^2))
+
+  # Read arrays of data
+  # The adiabatic .osc has numDatosCapa data per shell and 5 data per line
+  numLineas <- ceiling(numDatosCapa/5)
+
+  for(i in m:1){
+    datos <- readBlock(cfile, numLineas)
+    e$r[i] <- datos[indices["r"]]
+    e$q[i] <- datos[indices["q"]]
+    e$temp[i] <- datos[indices["t"]]
+    e$p[i] <- datos[indices["p"]]
+    e$rho[i] <- datos[indices["rho"]]
+    e$gamma[i] <- datos[indices["gamma"]]
+    e$aast[i] <- datos[indices["aast"]]
+    e$rot[i] <- datos[indices["rot"]]
+  }
+
+  # Converstion of data to log
+  e$r <- log(e$r)
+  e$q <- log(attributes(e)$rmass * exp(e$q))
+  e$p <- log(e$p)
+  e$rho <- log(e$rho)
+
+  # Complete data of the model
+  rel_t_r <- splinefun(exp(e$r),e$temp)
+  teff <- rel_t_r(attributes(e)$rfotom)
+  teff <- log10(teff)
+  e$rr <- exp(e$r)/attributes(e)$rfotom
+  e$c1 <- exp(3*e$r-e$q)*attributes(e)$rmass/(attributes(e)$rfotom^3)
+  e$cs2 <- e$gamma*exp(e$p-e$rho)
+  e$vg <- constants$gk*exp(e$q-e$r)/e$cs2
+  e$u <- 4*pi*exp(e$rho+3*e$r-e$q)
+  e$grav <- constants$gk*exp(e$q-2*e$r)
+  e$rn2 <- e$grav*e$aast/exp(e$r)
+  e$rnn <- e$rn2*(attributes(e)$rfotom^3)/(constants$gk*attributes(e)$rmass)
+
+  # Complete attributes
+  attr(e, "teff") <- teff
+  attr(e, "ct1") <- e$c1[1]
+  attr(e, "R") <- e$r[m]
+  attr(e, "version") <- version
+  attr(e, "code") <- driver
+  attr(e, "nshell") <- m + 1
+
+
+  # end
+  close(cfile)
+  return(e)
+
 
 }
 
