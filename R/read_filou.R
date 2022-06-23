1ba827aeec1a2d102d7062d85036292e91a8d61c
Juan Carlos Suarez <juancasy@gmail.com>
2022-06-24 00:35:37 +0200

    Update to version 0.0.0.9010 Include Imports dplyr in DESCRIPTION Update read_filou to properly read headers Update doc of l_sep to correct test decription.

--- R/read_filou.R
+++ R/read_filou.R
@@ -7,16 +7,15 @@
 #'
 #'
 #' @param filename String, scalar. Input FILOU oscillation file (.frq)
-#' @param header Logic. If TRUE then header of file is read (default F)
 #' @return A column-named dataframe containing the following data
-#'         l: the mode degree
-#'         n: the mode radial node
-#'         w2: squared oscillation frequency normalized to GMR
-#'         sig: oscillation frequency in microHz
-#'         fc: oscillation period in days
-#'         No: oscillation constant
-#'         pc: number of nodes as P mode
-#'         I: number of nodes as G mode
+#'         l: (integer) mode degree
+#'         n: (integer) mode radial node
+#'         w2: (double float) squared oscillation frequency normalized to GMR
+#'         sig: (double float) oscillation frequency in microHz
+#'         fc: (integer) Id of the mode with which is coupled
+#'         No: Mode Id
+#'         pc: (double float) coupling coefficient
+#'         I: (double float) mode inertia
 #'
 #'         All data included in file header is stored as attributes. To see all
 #'         attributes use:
@@ -38,8 +37,7 @@
 #'     \code{\link{read_graco}}, and \code{\link{read_gyre}}
 #'
 #'
-read_filou <- function(filename = NULL,
-                       header = FALSE) {
+read_filou <- function(filename = NULL) {
 
 # Local function to transform character version to integer
   ver_to_number <- function(x){
@@ -66,8 +64,12 @@ read_filou <- function(filename = NULL,
     sep = "",
     skip = no_skip,
     stringsAsFactors = FALSE,
-    col.names = c("n", "l", "m", "sig", "fc", "No",
-                  "pc", "I0")
+    col.names = c("n", "l", "m",
+                  "sig", "fc", "No",
+                  "pc", "I0"),
+    colClasses = c("integer", "integer", "integer",
+                   "numeric", "integer", "integer",
+                   "numeric", "numeric")
   )
 
   if (num_ver <= 3230){
@@ -79,18 +81,21 @@ read_filou <- function(filename = NULL,
     attr(data,"nshell") <- as.numeric(header[14])
     attr(data,"bso") <- as.numeric(header[15])
     attr(data,"modes") <- header[16]
-    attr(data,"mass") <- as.numeric(header[23])
-    attr(data,"radius") <- as.numeric(header[24])
-    attr(data,"lum") <- as.numeric(header[25])
-    attr(data,"teff") <- as.numeric(header[26])
+    attr(data,"mass") <- as.numeric(header[22])
+    attr(data,"radius") <- as.numeric(header[23])
+    attr(data,"lum") <- as.numeric(header[24])
+    attr(data,"teff") <- as.numeric(header[25])
+
     attr(data,"xc") <- as.numeric(header[30])
     attr(data,"logg") <- as.numeric(header[31])
     attr(data,"age") <- as.numeric(header[32])
     attr(data,"wroti") <- as.numeric(header[33])
+
     attr(data,"wrots") <- as.numeric(header[38])
     attr(data,"epsilon") <- as.numeric(header[39])
     attr(data,"nuroti") <- as.numeric(header[40])
     attr(data,"nurots") <- as.numeric(header[41])
+
     attr(data,"gmr3") <- as.numeric(header[48])
     attr(data,"numin") <- as.numeric(header[49])
     attr(data,"numax") <- as.numeric(header[50])
@@ -120,24 +125,27 @@ read_filou <- function(filename = NULL,
     attr(data,"x0") <- as.numeric(header[33])
     attr(data,"z0") <- as.numeric(header[34])
     attr(data,"age") <- as.numeric(header[35])
-    attr(data,"epsilon") <- as.numeric(header[39])
-    attr(data,"nuroti") <- as.numeric(header[40])
-    attr(data,"nurots") <- as.numeric(header[41])
-    attr(data,"gmr3") <- as.numeric(header[48])
-    attr(data,"numin") <- as.numeric(header[49])
-    attr(data,"numax") <- as.numeric(header[50])
-    attr(data,"lmin") <- as.numeric(header[51])
-    attr(data,"lmax") <- as.numeric(header[52])
-    attr(data,"wnorm") <- as.numeric(header[57])
-    attr(data,"nufund") <- as.numeric(header[58])
-    attr(data,"nuco") <- as.numeric(header[59])
-    attr(data,"vais") <- as.numeric(header[60])
-    attr(data,"nucoup") <- as.numeric(header[63])
-    attr(data,"ncoup") <- as.numeric(header[64])
+    attr(data,"wrots") <- as.numeric(header[40])
+    attr(data,"epsilon") <- as.numeric(header[41])
+    attr(data,"nuroti") <- as.numeric(header[42])
+    attr(data,"nurots") <- as.numeric(header[43])
+
+    attr(data,"gmr3") <- as.numeric(header[50])
+    attr(data,"numin") <- as.numeric(header[51])
+    attr(data,"numax") <- as.numeric(header[52])
+    attr(data,"lmin") <- as.numeric(header[53])
+    attr(data,"lmax") <- as.numeric(header[54])
+
+    attr(data,"wnorm") <- as.numeric(header[59])
+    attr(data,"nufund") <- as.numeric(header[60])
+    attr(data,"nuco") <- as.numeric(header[61])
+    attr(data,"vais") <- as.numeric(header[62])
+
+    attr(data,"nucoup") <- as.numeric(header[65])
+    attr(data,"ncoup") <- as.numeric(header[66])
   }
 
   data$amp <- 1 # This adds the amplitude variable to the dataframe
-  #return(data[, c("n", "l", "sig")])
   return(data)
 }
 
